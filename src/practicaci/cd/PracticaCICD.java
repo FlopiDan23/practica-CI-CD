323877133018937c70f12e11637ec96cde0b43f8
flopiDan23 <florencia.dannunzio@gmail.com>
2023-10-05 20:38:13 -0300

    mensaje agregado

--- src/practicaci/cd/PracticaCICD.java
+++ src/practicaci/cd/PracticaCICD.java
@@ -15,7 +15,9 @@ public class PracticaCICD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Se ha relizado una confirmacion");
+        
     }
     
 }
